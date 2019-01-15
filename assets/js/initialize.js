9147c34745177e0997789810f929e70618860a0f
eudesroger <eudessilva@mangue3.com>
2019-01-15 16:32:25 -0300

    adjustment

--- assets/js/initialize.js
+++ assets/js/initialize.js
@@ -4,9 +4,16 @@ $(document).ready(function() {
 });
 
 var map;
+var marker;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
+        center: {lat: -8.0633253, lng: -34.8745272},
+        zoom: 15
+    });
+
+    marker = new google.maps.Marker({
+        position: {lat: -8.0633253, lng: -34.8745272},
+        map: map,
+        title: 'Jogga'
     });
 }
